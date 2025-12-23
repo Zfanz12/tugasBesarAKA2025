0052b6b2cb6f79ec9575be15c1c89e00ec78d8fc
Zfanz12 <zulfanfalah@gmail.com>
2025-12-23 21:22:13 +0700

    done add feature

--- sortingStokBarang.py
+++ sortingStokBarang.py
@@ -3,6 +3,8 @@ from tkinter import ttk, messagebox
 import time
 import matplotlib.pyplot as plt
 import sys
+import random
+
 
 sys.setrecursionlimit(2000)
 
@@ -15,6 +17,8 @@ def bubble_sort_iteratif(data):
         for j in range(n - i - 1):
             if data[j][1] > data[j + 1][1]:
                 data[j], data[j + 1] = data[j + 1], data[j]
+
+
 
 def bubble_sort_rekursif(data, n):
     if n <= 1:
@@ -43,6 +47,17 @@ def update_tabel(data):
     for item in data:
         tabel.insert("", "end", values=item)
 
+def numeric(P):
+    return P.isdigit() or P == ""
+
+def barang_random(jumlah=10):
+    for i in range (jumlah):
+        nama = f"Barang-{len(data_barang)+1}"
+        stok = random.randint(1, 100)
+
+        data_barang.append((nama, stok))
+        tabel.insert("", "end", values=(nama, stok))
+
 def sort_iteratif():
     if not data_barang: return
     data = list(data_barang)
@@ -56,6 +71,7 @@ def sort_iteratif():
     update_tabel(data)
     hasil_eksekusi["Iteratif"][n] = waktu
     print(f"Iteratif: {n} data | {waktu} ns")
+
 
 def sort_rekursif():
     if not data_barang: return
@@ -100,13 +116,16 @@ tk.Label(frame_input, text="Nama Barang:").grid(row=0, column=0, sticky="w")
 entry_nama = tk.Entry(frame_input)
 entry_nama.grid(row=0, column=1, padx=5, pady=2)
 
+vcmd = (root.register(numeric), '%P')
+
 tk.Label(frame_input, text="Stok:").grid(row=1, column=0, sticky="w")
-entry_stok = tk.Entry(frame_input)
+entry_stok = tk.Entry(frame_input, validate="key", validatecommand=vcmd)
 entry_stok.grid(row=1, column=1, padx=5, pady=2)
 
 btn_tambah = tk.Button(frame_input, text="Tambah ke Tabel", command=tambah_data, bg="#e1e1e1")
 btn_tambah.grid(row=2, columnspan=2, pady=10, sticky="ew")
-
+btn_random = tk.Button(frame_input, text="Tambah Data Random", command=barang_random, bg="#e1e1e1")
+btn_random.grid(row=3, columnspan=2, pady=5, sticky="ew")
 frame_tabel = tk.Frame(root)
 frame_tabel.pack(padx=10, pady=5)
 
@@ -127,5 +146,6 @@ frame_aksi.pack(pady=10)
 tk.Button(frame_aksi, text="Sort Iteratif", command=sort_iteratif, width=15).grid(row=0, column=0, padx=5)
 tk.Button(frame_aksi, text="Sort Rekursif", command=sort_rekursif, width=15).grid(row=0, column=1, padx=5)
 tk.Button(root, text="Tampilkan Grafik Perbandingan", command=tampilkan_grafik, bg="#add8e6").pack(pady=5, fill="x", padx=10)
+
 
 root.mainloop()
